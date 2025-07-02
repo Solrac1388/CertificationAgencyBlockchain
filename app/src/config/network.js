6babd18dab981c16e706c03ec3928b47c7d39ed4
Solrac1388 <clsatocha@gmail.com>
2025-07-02 21:53:09 +0200

    Adicion de nodes_ip.txt

--- app/src/config/network.js
+++ app/src/config/network.js
@@ -12,16 +12,7 @@ export const NETWORK_CONFIG = {
   protocolVersion: '1.0',
   
   // SOLO nodos hardcodeados - NO hay soporte para archivo externo
-  trustedNodes: [
-    '192.168.1.100:8333',
-    '192.168.1.101:8333',
-    '10.0.0.50:8333',
-    '10.0.0.51:8333',
-    'node1.blockchain.local:8333',
-    'node2.blockchain.local:8333',
-    '172.16.0.10:8333',
-    '172.16.0.11:8333',
-  ],
+  trustedNodes: TRUSTED_NODES_PLACEHOLDER,
   
   requestTimeout: 30000,
   broadcastInterval: 5000,
